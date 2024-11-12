0ba5e715e008ed6d5e161b2564f2e896c61c8e69
Darshan <Admin@LAPTOP-6GE9GT90>
2024-11-12 20:58:16 +0530

    New Commit

---
+++ SpringPractice/src/main/java/com/Darshan/SpringPractice/App.java
@@ -0,0 +1,19 @@
+package com.Darshan.SpringPractice;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");  // To work with Spring Container we need to use Application Container 
+		// which is subset off BeanFactory as we know Spring Container objects, configures and assembles their dependencies
+
+        TCSers tcs = (TCSers) context.getBean("tcsCoders");
+    }
+}
---
+++ SpringPractice/src/main/java/com/Darshan/SpringPractice/TCSers.java
@@ -0,0 +1,11 @@
+package com.Darshan.SpringPractice;
+
+public class TCSers {
+
+	public void code() {
+		System.out.println("Lets Do Coding...");
+	}
+	
+	
+	
+}
